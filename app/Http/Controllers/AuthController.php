d848e05a502c54375ce524c5083e00cca7abcdd3
Mehdi-133 <mehdikarbitou@gmail.com>
2026-03-10 13:11:37 +0100

    BA-14 User Login

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers;
 
 use App\Models\User;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Hash;
 
 class AuthController extends Controller
 {
@@ -17,10 +18,39 @@ class AuthController extends Controller
 
         $user = User::create($fields);
         $token = $user->createToken($request->name);
-      
+
         return [
             'user' => $user,
             'token' => $token->plainTextToken
         ];
     }
+
+
+    public function login(Request $request)
+    {
+        $fields = $request->validate([
+            'email'     => 'required|email',
+            'password'  => 'required',
+        ]);
+
+        $user = User::where('email', $request->email)->first();
+
+        if (!$user || !Hash::check($request->password, $$user->password)) {
+
+            return [
+                'message' => 'Invalid Credentials'
+            ];
+        }
+
+        $token = $user->createToken($request->email);
+
+        return [
+            'user' => $user,
+            'token' => $token->plainTextToken
+        ];
+    }
+
+    
 }
+
+
--- routes/api.php
+++ routes/api.php
@@ -10,5 +10,6 @@ use Illuminate\Support\Facades\Route;
 
 
 Route::post('/register' , [AuthController::class , 'register']);
+Route::post('/login' , [AuthController::class , 'login']);
 
 
